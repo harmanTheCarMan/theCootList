138df89c14324b8f480acf21a2ca621184f33c5f
John Hopkins <johnthopkins@gmail.com>
2016-09-07 09:24:14 -0700

    set up routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TABR' });
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('create_task', { title: 'New Task' });
+});
+
+router.post('/create', function(req, res, next) {
+  res.redirect('/', { title: 'New Task' });
+});
+
+router.get('/update/:id', function(req, res, next) {
+  res.render('update_task', { title: 'Update' });
+});
+
+router.post('/update/:id', function(req, res, next) {
+  res.redirect('/', { title: 'Update' });
 });
 
 module.exports = router;
