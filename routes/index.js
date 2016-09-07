cb4de12d7c32a591005bbedf9396fe279a199c78
Harman Singh <harmanlearns@gmail.com>
2016-09-07 16:33:54 -0700

    Fixed merge conflicts for displaying logged in status

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../database/db').User
 
 const authorize = require('../authentication/passport').authorize
 
 /* GET home page. */
 router.get('/', authorize, (req, res, next) => {
-  if (req.session.passport.user) {
-    res.render('index', { title: 'TABR' })
-  }
-  else res.redirect('/users/login')
+  res.render('index', { title: 'TABR', user:req.user })
+  console.log('user', req.user)
 })
+
+router.get('/create', authorize, (req, res, next) => {
+  res.render('create_task', { title: 'New Task' })
+})
+
+router.post('/create', authorize, (req, res, next) => {
+  res.redirect('/', { title: 'New Task' })
+})
+
+router.get('/update/:id', authorize, (req, res, next) => {
+  res.render('update_task', { title: 'Update' })
+})
+
+router.post('/update/:id', authorize, (req, res, next) => {
+  res.redirect('/', { title: 'Update' })
+})
+
+
 
 module.exports = router
