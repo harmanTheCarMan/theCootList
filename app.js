cab6b6a7ee0e01f770e8a6f6227ce3623e118230
Harman Singh <harmanlearns@gmail.com>
2016-09-07 12:06:21 -0700

    Passport implemented

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express')
+const session = require('express-session')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const passport = require( './authentication/passport' ).passport
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const routes = require('./routes/index')
+const users = require('./routes/users')
 
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use( session({ secret: 'slartibartfast', saveUninitialized: false, resave: false }))
+app.use( passport.initialize() )
+app.use( passport.session() )
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes)
+app.use('/users', users)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
@@ -38,23 +43,23 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
 
-module.exports = app;
+module.exports = app
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const User = require('../database/db').User
+const passport = require('../authentication/passport').passport
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
+const loginRedirects = {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}
 
-/* Register new user. */
-router.get('/register', (req, res, next) => {
-  res.render('register');
-});
+router.get( '/register', (req, res, next) => {
+  res.render('register')
+})
 
-/* Log in user. */
-router.get('/login', (req, res, next) => {
-  res.render('login');
-});
+router.post( '/register', (req, res) => {
+  const { email, password } = req.body
 
-module.exports = router;
+  User.create( email, password )
+    .then( user => {
+      req.login( user, error => {
+        if( error ) {
+          return next( error )
+        }
+
+        res.redirect('/')
+      })
+    })
+})
+
+router.get( '/login', (req, res, next) => {
+  res.render('login')
+})
+
+router.post('/login', passport.authenticate( 'local', loginRedirects ))
+
+router.get( '/logout', (request, response) => {
+  request.logout()
+  response.redirect( '/' )
+})
+
+module.exports = router
