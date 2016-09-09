31f92378639fb92973a7a8d460fc74535b5b06d8
Harman Singh <harmanlearns@gmail.com>
2016-09-08 17:00:29 -0700

    Add to tab works

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,9 +5,7 @@ const Task = require('../database/db').Task
 const authorize = require('../authentication/passport').authorize
 
 router.post( '/', authorize, (request, response) => {
-  //Need to change tab_id to correspond to current tab so that tasks can be added for that specific tab instead of only for the default user id only... not sure how to implement
-  const tab_id = request.user.id
-  console.log(tab_id)
+  const tab_id = request.body[ 'tab-id' ]
   const description = request.body[ 'new-task' ]
 
   Task.create( tab_id, description )
